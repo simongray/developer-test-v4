91eabc8c031359749f7dd7f163de63cc060017c4
Simon Gray <simongray@gmail.com>
2018-01-29 19:47:33 +0100

    added styles for text bubbles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TextBubble = () => (
+  <div class="text-bubble own">
+    <div class="header">
+      <span class="author">Peer #1 wrote</span> <span class="time">4th July 14.21</span>
+    </div>
+    <p>Bla bla bla bla</p>
+    <div class="footer">
+      David changed the grade on question 2 from <span class="grade">No answer</span> to <span class="grade">Exceeds expectations</span>
+    </div>
+  </div>
+)
+
 const FeedbackForm = () => (
   <form class="feedback-form">
     <textarea placeholder="Type your message here" />
@@ -14,6 +26,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <TextBubble />
+        <TextBubble />
         <FeedbackForm />
       </div>
     );
